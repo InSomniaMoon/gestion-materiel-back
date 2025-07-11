055606c6747574439849a0ec79d311c872e73b60
Pierre Leroyer <pierre.leroyer69@gmail.com>
2025-07-11 17:21:54 +0200

    feat: add Unit model and migration; define relationships with Group and User models

---
+++ database/migrations/2025_07_11_143337_add_group_units.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('units', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('group_id')->references('id')->on('groups')->constrained()->onDelete('cascade');
+      $table->foreignId('responsible_id')->nullable()->references('id')->on('users')->constrained()->onDelete('set null');
+      $table->string('name');
+      $table->string('color', length: 7);
+      $table->timestamps();
+    });
+
+    Schema::create('unit_users', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('unit_id')->references('id')->on('units')->constrained()->onDelete('cascade');
+      $table->foreignId('user_id')->references('id')->on('users')->constrained()->onDelete('cascade');
+    });
+
+    // the table subscriptions will have a foreign key to the units table
+    Schema::table('item_subscriptions', function (Blueprint $table) {
+      $table->foreignId('unit_id')->nullable()->references('id')->on('units')->constrained()->onDelete('set null');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('units');
+    Schema::dropIfExists('unit_users');
+  }
+};
